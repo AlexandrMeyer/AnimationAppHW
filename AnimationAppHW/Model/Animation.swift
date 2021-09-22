0e06805867e9901166ab5be5010e8374f52b6fd5
AlexandrMeyer <alexandrmeyerr@gmail.com>
2021-09-22 23:21:42 +0300

    not all finish

--- AnimationAppHW/Model/Animation.swift
+++ AnimationAppHW/Model/Animation.swift
@@ -5,12 +5,31 @@
 //  Created by Александр on 21.09.21.
 //
 
-import Foundation
-
 struct Animation {
     let preset: String
     let curve: String
     let force: Float
     let duration: Float
     let delay: Float
+    
+    static func getAnimation() -> [Animation] {
+        var animations = [Animation]()
+
+        let preset = DataManager.shared.preset.shuffled()
+        let curve = DataManager.shared.curve.shuffled()
+        
+        for presetIndex in 0..<preset.count {
+            let animation = Animation(
+                preset: preset[presetIndex],
+                curve: curve[Int.random(in: 0..<curve.count)],
+                force: Float.random(in: 0.3...1.2),
+                duration: Float.random(in: 1...2),
+                delay: Float.random(in: 0.2...0.8)
+            )
+            animations.append(animation)
+        }
+        return animations
+    }
 }
+
+
--- AnimationAppHW/ViewControllers/AnimationViewController.swift
+++ AnimationAppHW/ViewControllers/AnimationViewController.swift
@@ -9,6 +9,9 @@ import Spring
 
 class AnimationViewController: UIViewController {
     
+    // MARK: - Outlets
+    @IBOutlet var animationView: SpringView!
+    
     @IBOutlet var presetLabel: UILabel!
     @IBOutlet var curveLabel: UILabel!
     @IBOutlet var forceLabel: UILabel!
@@ -17,10 +20,46 @@ class AnimationViewController: UIViewController {
     
     @IBOutlet var animationButton: UIButton!
     
-    @IBAction func runAnimationButtonTapped(_ sender: SpringButton) {
-        
+    // MARK: - Private properties
+    private let animations = Animation.getAnimation()
+    private var animationIndex: Int {
+        Int.random(in: 0..<animations.count)
+    }
+    private var currentAnimation: Animation {
+        animations[animationIndex]
     }
     
+    // MARK: - Override Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateUI(for: currentAnimation)
+    }
+    
+    // MARK: - IBActions
+    @IBAction func runAnimationButtonTapped(_ sender: SpringButton) {
+        currentAnimation(from: currentAnimation)
+    }
+    
+    // MARK: - Private Methods
+    private func currentAnimation(from animation: Animation) {
+        animationView.animation = animation.preset
+        animationView.curve = animation.curve
+        animationView.force = CGFloat(animation.force)
+        animationView.duration = CGFloat(animation.duration)
+        animationView.delay = CGFloat(animation.delay)
+        animationView.animate()
 
+        animationButton.setTitle("Run \(animation.preset)", for: .normal)
+        
+        updateUI(for: animation)
+    }
+
+    private func updateUI(for animation: Animation) {
+        presetLabel.text = animation.preset
+        curveLabel.text = animation.curve
+        forceLabel.text = String(format: "%.2f", animation.force)
+        durationLabel.text = String(format: "%.2f", animation.duration)
+        delayLabel.text = String(format: "%.2f", animation.delay)
+    }
 }
 
